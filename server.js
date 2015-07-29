26fff970469f2300817b9299733cba7bb7fb765d
Kasper Nissen <kaspernissen@gmail.com>
2015-07-29 14:44:52 +0200

    Login form added

--- server.js
+++ server.js
@@ -4,9 +4,15 @@
 // =============================================================================
 
 // Setup the packages that we need
-var express    = require('express');        // call express
+var express    = require('express'), 
+	exphbs     = require('express-handlebars');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
+var url = require('url') ;
+
+// Setup view engine
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 // Hardcoded stuff
 var clientId = 'A22d2fg224h98k8D7HH21';
@@ -26,17 +32,21 @@ var port = process.env.PORT || 8080;        // set our port
 var router = express.Router();              // get an instance of the express Router
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });   
+router.get('/login', function(req, res) {
+	var receivedClientId = url.parse(req.url,true).query.clientId;
+	console.log('RClientId: ' + receivedClientId);
+    res.render('login');   
 });
 
-router.post('/login', function(req, res) {
-	if (req.body.clientId === clientId) {
-		res.json({ message: 'Authenticated', token: token, expires: experation});  
-	}
-	else {
-		res.json({ message: 'Go away!' }); 
-	}	
+router.post('/authorize', function(req, res) {
+	console.log(req.body.username);
+	console.log(req.body.password);
+	// if (req.body.clientId === clientId) {
+	// 	res.json({ message: 'Authenticated', token: token, expires: experation});  
+	// }
+	// else {
+	// 	res.json({ message: 'Go away!' }); 
+	// }	
 })
 
 
@@ -51,7 +61,7 @@ router.post('/token-validation', function(req, res) {
 
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
-app.use('/api', router);
+app.use('', router);
 
 // START THE SERVER
 // =============================================================================
